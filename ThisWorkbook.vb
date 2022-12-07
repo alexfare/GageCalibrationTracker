9fa1aabe9957f8447f09b2de5f61a72aaad0bd60
Alex <40654995+alexfare@users.noreply.github.com>
2022-12-07 13:17:32 -0500

    v1.1

---
+++ ThisWorkbook.vb
@@ -0,0 +1,13 @@
+v1.1
+
+Private Sub Workbook_Open()
+
+'/ Require Login to open /
+    'Worksheets("Login").Activate
+    'LoginForm.Show
+    
+'/ Skip Login /
+    Worksheets("CreatedByAlexFare").Activate
+    UserForm1.Show
+    
+End Sub
