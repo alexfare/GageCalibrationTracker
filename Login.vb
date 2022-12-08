f7237481f3f544b52afafdf48c89c15c981e3eb2
Alex <40654995+alexfare@users.noreply.github.com>
2022-12-08 07:18:42 -0500

    v1.1.0
    ' Logging in now redirects to Admin Panel

--- Login.vb
+++ Login.vb
@@ -1,8 +1,8 @@
 'Login - Created by Alex Fare
-'Version - 1.0.3
-'Updated - 12/07/2022
+'Version - 1.1.0
+'Updated - 12/**/2022
 '
-'
+' Logging in now redirects to Admin Panel
 ' Added Failed Login Warning
 ' Removed unused code
 '
@@ -22,11 +22,12 @@ End If
 If inputUser.Value = "Admin" And inputPass.Value = "Admin" Then
 Unload Me
 Sheets("CreatedByAlexFare").Activate
-UserForm1.Show
+' UserForm1.Show
+AdminForm.Show
 Else
 MsgBox "Login Failed, Wrong Password or Username.", vbInformation, ""
 End If
 
 End Sub
 
-
+' Change Login to be Admin Panel only
