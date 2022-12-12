db00aba46044990b2a518d92fd954417e0b7934a
Alex <40654995+alexfare@users.noreply.github.com>
2022-12-12 07:42:28 -0500

    v1.2.0
    ' Added multiple logins (Wrong username will cause crash)

--- Login.vb
+++ Login.vb
@@ -1,7 +1,8 @@
 'Login - Created by Alex Fare
-'Version - 1.1.0
-'Updated - 12/08/2022
+'Version - 1.2.0
+'Updated - 12/12/2022
 '
+' Added multiple logins (Wrong username will cause crash)
 ' Logging in now redirects to Admin Panel
 ' Added Failed Login Warning
 ' Removed unused code
@@ -25,9 +26,29 @@ Sheets("CreatedByAlexFare").Activate
 ' UserForm1.Show
 AdminForm.Show
 Else
-MsgBox "Login Failed, Wrong Password or Username.", vbInformation, ""
+
+Dim inputUsername As String
+Dim Password As Variant
+
+inputUsername = inputUser.Value
+Password = Application.WorksheetFunction.VLookup(inputUsername, Sheets("Credentials").Range("A:B"), 2, 0)
+
+If Password <> inputPass.Value Then
+MsgBox "Login Failed, Wrong Password or Username.", vbInformation, "Wrong Password"
+Exit Sub
 End If
+
+If Password = inputPass.Value Then
+Unload Me
+Sheets("CreatedByAlexFare").Activate
+AdminForm.Show
+End If
+End If
+
 
 End Sub
 
 ' Change Login to be Admin Panel only
+
+
+
