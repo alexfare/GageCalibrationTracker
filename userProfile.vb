1219ea09bd2f3a975d9973abb40924674e808fd8
Alex <40654995+alexfare@users.noreply.github.com>
2023-04-11 07:42:23 -0400

    Delete userProfile.vb

--- userProfile.vb
+++
@@ -1,6 +0,0 @@
-Private Sub UserForm_Activate()
-'/Positioning /'
-    Me.Left = Application.Left + (0.5 * Application.Width) - (0.5 * Me.Width)
-    Me.Top = Application.Top + (0.5 * Application.Height) - (0.5 * Me.Height)
-'/End Positioning /'
-End Sub
